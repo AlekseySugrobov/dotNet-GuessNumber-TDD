7801eca531fcdd18402e017c17a344e433b61b5f
AlekseySugrobov <Aleksey.Sugrobov@gmail.com>
2017-08-29 11:56:55 +0500

    translate console messages

--- GuessNumberConsole/Program.cs
+++ GuessNumberConsole/Program.cs
@@ -15,21 +15,21 @@ namespace GuessNumberConsole
             do
             {
                 playTheGame();
-                Console.WriteLine(@"Еще раз? [N = нет][Любая другая клавиша = да]");
+                Console.WriteLine(@"Another one? [N = no][Another key = yes]");
                 answer = Console.ReadLine();
             } while (answer.ToLower() != "n");
-            Console.WriteLine("Игра окончена.");
+            Console.WriteLine("Game is finished.");
             Console.ReadLine();
         }
 
         private static void playTheGame()
         {
             var guessNumber = new GuessNumber();
-            Console.WriteLine("Число загадано. Игра начата.");
+            Console.WriteLine("Game is start.");
             do
             {
-                Console.WriteLine($"Попытка {guessNumber.CountTries + 1} из {guessNumber.GetMaxTries()}");
-                Console.WriteLine("Введите число:");
+                Console.WriteLine($"Try {guessNumber.CountTries + 1} from {guessNumber.GetMaxTries()}");
+                Console.WriteLine("Enter number:");
                 try
                 {
                     tryingToGuessNumber(guessNumber);
@@ -40,8 +40,8 @@ namespace GuessNumberConsole
                 }
             } while (!guessNumber.IsGameEnd);
             Console.WriteLine(guessNumber.IsWon
-                ? $"Поздравляю! Вы угадали загаданное число! Количество попыток: {guessNumber.CountTries}"
-                : $"Вы проиграли. Загаданное число {guessNumber.GetGuessedNumber()} не было угадано!");
+                ? $"Congratulation! Enterd number is correct! Tries count: {guessNumber.CountTries}"
+                : $"You lose. Guessed number is {guessNumber.GetGuessedNumber()}");
         }
 
         private static void tryingToGuessNumber(GuessNumber guessNumber)
@@ -52,17 +52,17 @@ namespace GuessNumberConsole
                 var result = guessNumber.CompareWithGuessedNumber(enteredNumber);
                 if (result == CompareResults.Great)
                 {
-                    Console.WriteLine("Введенное число больше загаданного");
+                    Console.WriteLine("Entered number is greater than guessed number");
                 }
                 else if (result == CompareResults.Less)
                 {
-                    Console.WriteLine("Введенное число меньше загаданного");
+                    Console.WriteLine("Entered number is less than guessed nubmer");
                 }
             }
             else
             {
                 Console.WriteLine(
-                    $"Загаданное число находится в диапазоне от {guessNumber.GetNumberMinValue()} до {guessNumber.GetNumberMaxValue()}");
+                    $"Guessed number is from {guessNumber.GetNumberMinValue()} to {guessNumber.GetNumberMaxValue()}");
             }
         }
     }
